5ecbe91d24978397b7dd588f616a1f8851c80546
liron <kinglir@gmail.com>
2020-04-05 19:13:43 +0300

    lambda function implementation

--- nextpy 1.3.2.py
+++ nextpy 1.3.2.py
@@ -1,12 +1,9 @@
 # 1.3.2
 ################
 
-
-def is_prime(number):
-    """Check if a number is a prime number"""
-    return 0 not in [number % i for i in list(range(2, (number//2)+1))]
-
+"""Check if a number is a prime number"""
+is_prime = lambda number: all([number % i for i in list(range(2, (number // 2) + 1))])
 
 if __name__ == "__main__":
-    test_input = 29
+    test_input = 107
     print(is_prime(test_input))
