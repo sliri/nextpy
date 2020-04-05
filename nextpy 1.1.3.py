5f482ce6b51553cccda0e47119d5a191e686ed5d
liron <kinglir@gmail.com>
2020-04-05 14:48:50 +0300

    added documentation

--- nextpy 1.1.3.py
+++ nextpy 1.1.3.py
@@ -3,10 +3,12 @@
 
 
 def modulo4_checker(num):
+    """Check if a number is divided by 4"""
     return num % 4 == 0
 
 
 def four_dividers(num):
+    """ Run over a range of numbers(from 1 to number).Return a new list with the numbers that are divided by 4 """
     return list(filter(modulo4_checker, list(range(1, num+1))))
 
 
