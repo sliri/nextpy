d03f6c9d8639e2013f2f91d81be2297b725fa994
liron <kinglir@gmail.com>
2020-04-05 14:55:15 +0300

    added documentation

--- nextpy 1.1.4.py
+++ nextpy 1.1.4.py
@@ -5,18 +5,22 @@ import functools
 
 
 def list2str(num):
+    """Convert an integer to a list of  its digits, each of which is a string"""
     return list(str(num))
 
 
 def str2int(num):
+    """Convert an integer to a list of digits, each of which is an integer"""
     return list(map(int, num))
 
 
 def add(x, y):
+    """Add two numbers"""
     return x + y
 
 
 def sum_of_digits(number):
+    """Get an integer, return the sum of its digits"""
     return functools.reduce(add, (str2int(list2str(number))))
 
 
