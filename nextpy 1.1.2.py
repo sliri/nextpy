6da6cadf89b0e5cb67e8383314a98781d48a38f1
liron <kinglir@gmail.com>
2020-04-05 14:45:16 +0300

    added documentation

--- nextpy 1.1.2.py
+++ nextpy 1.1.2.py
@@ -3,10 +3,12 @@
 
 
 def double_string(my_str):
+    """Get a string ,return a "double" (origin+replica) string """
     return 2 * my_str
 
 
 def double_letter(my_str):
+    """Run over all letters in a string, double(origin+replica) each letter, return the new created string  """
     return ''.join((map(double_string, my_str)))
 
 
