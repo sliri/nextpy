83169b076a6dcfdb700f388fbfe1ede6a39a29b0
liron <kinglir@gmail.com>
2020-04-10 18:54:18 +0300

    initial version

---
+++ nextpy 3.3.2.py
@@ -0,0 +1,31 @@
+# 3.2.5
+################
+
+
+class UnderAge(Exception):
+    """An under age exception class"""
+
+    def __init__(self, age):
+        """Initialize the age"""
+        self.age = age
+
+    def __str__(self):
+        """The class description string"""
+        return "Your age is {}, wait another {} years so you can come to" \
+               "Ido's birthday!".format(self.age, (18 - self.age))
+
+
+def send_invitation(name, age):
+    """Send invitation to a person if he/she is under 18"""
+    try:
+        if int(age) < 18:
+            raise UnderAge(age)
+    except UnderAge as e:
+        print('Function expected a positive integer under 18 and got %s' %age)
+    else:
+        print("You should send an invite to " + name)
+
+
+if __name__ == "__main__":
+    send_invitation('Tali', 20)
+    send_invitation('Tali', 17)
