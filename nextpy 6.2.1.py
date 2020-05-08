140dcfe22c22293f1b26ad4a9508f059f24c90e0
liron <kinglir@gmail.com>
2020-05-08 19:08:05 +0300

    initial version

---
+++ nextpy 6.2.1.py
@@ -0,0 +1,11 @@
+# 6.2.1
+################
+
+
+import file1
+import file2
+
+BirthdayCard = file2.BirthdayCard(25)
+GreetingCard = file1.GreetingCard()
+BirthdayCard.greeting_msg()
+GreetingCard.greeting_msg()
